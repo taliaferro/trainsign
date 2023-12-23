c272a971d825262ca6295af468fc55ec59c4af79
James Taliaferro <taliaferro1@llnl.gov>
2023-12-22 19:56:44 -0700

    correct name typo

--- src/trainsign/cli.py
+++ src/trainsign/cli.py
@@ -8,10 +8,11 @@ class TrainSignCLI:
     def __init__(self, config_path: os.PathLike = "./trainsign.yml"):
         self._config_path = config_path
 
-    def start_sign(self):
+    def run_sign(self):
         raise NotImplementedError()
 
     def systemd(self) -> str:
+        # TODO: run the service as the unprivileged user who installed it
         unit_template = """[Unit]
 Description=trainsign
 
